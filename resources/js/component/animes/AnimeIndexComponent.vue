eb3a37da9e92ab32a7884c4d69beb7dc03aafd88
Rezu0 <yunianrezky031@gmail.com>
2024-05-24 15:04:18 +0700

    configurasi untuk button dan fixed column di DataTable

--- resources/js/component/animes/AnimeIndexComponent.vue
+++ resources/js/component/animes/AnimeIndexComponent.vue
@@ -1,20 +1,25 @@
 <script setup>
-  import { ref, render, watchEffect } from 'vue';
+  import { ref, watchEffect } from 'vue';
+  import { toast } from 'vue-sonner';
   import DataTable from 'datatables.net-vue3';
   import DataTablesCore from 'datatables.net';
   import Fetching from '../../utils/handler-fetching';
   import Cookies from '../../utils/handler-cookies';
-  import { toast } from 'vue-sonner';
+  import FormatDate from '../../utils/handler-date';
 
   DataTable.use(DataTablesCore);
 
   const columns = [
+    {
+      data: 'uuid',
+      render: '#action',
+    },
     { 
       data: 'data.nama_anime',
       render: (data, type, row) => {
         return data?.romanji ? data?.romanji : data?.eng
       },
-      width: '400px'
+      width: '350px'
     },
     {
       data: 'data.source',
@@ -35,7 +40,44 @@
       width: '500px'
     },
     {
+      data: 'data.start_date',
+      render: (data, type, row) => {
+        return (!data) ? 'Belom dijadwalkan' : FormatDate.formatDateStart(data)
+      },
+      width: '300px'
+    },
+    {
+      data: 'data.status',
+      render: (data, type, row) => {
+        return `
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <span class="badge badge-primary">
+                  ${data.statusAiring}
+                </span>
+              </div>
+              <div class="col-md-12">
+                <span class="badge badge-danger">
+                  ${data.total_eps} Episode
+                </span>
+              </div>
+            </div>
+          </div>
+        `
+      }
+    },
+    {
+      data: 'created_at',
+      render: (data, type, row) => {
+        return FormatDate.formatDateOnline(data)
+      }
+    },
+    {
       data: 'whois.username_mal',
+      render: (data, type, row) => {
+        return `<span class="badge badge-success">${data}</span>`
+      },
       width: '200px'
     }
   ];
@@ -95,18 +137,31 @@
                 :options="{
                   responsive: true,
                   processing: true,
-                  pageLength: 5,
-                  lengthMenu: [ [5, 10, 25, 50, -1], [5, 10, 25, 50, 'All'] ],
+                  pageLength: 10,
+                  lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, 'All'] ],
                   scrollX: true,
-                  order: [[0]]
+                  scrollY: '570px',
+                  order: [[0]],
+                  fixedColumns: {
+                    leftColumns: 2,
+                    rightColumns: 0
+                  },
                 }"
               >
+                <template #action="props">
+                  <button class="btn btn-danger btn-sm" @click="() => toast.success(props.rowData.uuid)">Delete {{ props.rowData.uuid }}</button>
+                </template>
+
                 <thead>
                   <tr>
-                    <th>Nama Anime</th>
+                    <th>Action</th>
+                    <th>Name Anime</th>
                     <th>Source</th>
                     <th>Studio</th>
                     <th>Genres</th>
+                    <th>Start Date</th>
+                    <th>Status</th>
+                    <th>Created At</th>
                     <th>Admin</th>
                   </tr>
                 </thead>
@@ -153,6 +208,11 @@
 
 .table {
   overflow-x: auto;
+  overflow-y: auto;
   white-space: nowrap;
+}
+
+.dtfc-fixed-left {
+  z-index: 10;
 }
 </style>
