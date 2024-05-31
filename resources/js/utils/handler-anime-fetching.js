5c7f18b895d12ff9cb1a0a28b685e3291e179837
Rezu0 <yunianrezky031@gmail.com>
2024-05-31 20:31:40 +0700

    add new method untuk delete anime

--- resources/js/utils/handler-anime-fetching.js
+++ resources/js/utils/handler-anime-fetching.js
@@ -22,6 +22,33 @@ const createAnime = async (token, data) => {
   }
 }
 
+const softDeleteAnime = async (token, data) => {
+  const headerSoftDelete = new Headers();
+  headerSoftDelete.append("Content-Type", "application/json");
+  headerSoftDelete.append("Authorization", `Bearer ${token}`);
+
+  const dataBody = JSON.stringify({
+    animeUuid: data
+  })
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: headerSoftDelete,
+    body: dataBody,
+    redirect: "follow",
+  };
+
+  try {
+    const softDeleteAnime = await fetch(`${BASE_URL}/anime/delete`, requestOptions);
+    const result = await softDeleteAnime.json();
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
 export default {
-  createAnime
+  createAnime,
+  softDeleteAnime
 }
