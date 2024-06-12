a6114bb7126238da1ae1fe66970b62c14e1b3d8b
Rezu0 <yunianrezky031@gmail.com>
2024-06-12 21:28:17 +0700

    unit test php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,6 +12,9 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
+
+     use RefreshDatabase;
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
