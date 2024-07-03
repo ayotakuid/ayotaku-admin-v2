e50858a623e0f4e66310a9a72832a551e4b931bb
Rezu0 <yunianrezky031@gmail.com>
2024-07-03 23:30:08 +0700

    change icon untuk menu dashboard admin

--- resources/js/component/layouts/MenuComponent.vue
+++ resources/js/component/layouts/MenuComponent.vue
@@ -42,7 +42,7 @@ import { ref, watch } from 'vue';
                   <div class="menu-item">
                     <router-link to="/dashboard" class="menu-link" :class="{ 'active': active?.name === 'dashboard-page' }">
                         <span class="menu-bullet">
-                            <span class="bullet bullet-dot"></span>
+                          <i class="fa-solid fa-house"></i>
                         </span>
                         <span class="menu-title">Dashboard Admin</span>
                     </router-link>
@@ -68,14 +68,11 @@ import { ref, watch } from 'vue';
                   <span class="menu-icon">
                       <!--begin::Svg Icon | path: icons/duotune/abstract/abs014.svg-->
                       <span class="svg-icon svg-icon-2">
-                          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path opacity="0.3" d="M11.8 5.2L17.7 8.6V15.4L11.8 18.8L5.90001 15.4V8.6L11.8 5.2ZM11.8 2C11.5 2 11.2 2.1 11 2.2L3.8 6.4C3.3 6.7 3 7.3 3 7.9V16.2C3 16.8 3.3 17.4 3.8 17.7L11 21.9C11.3 22 11.5 22.1 11.8 22.1C12.1 22.1 12.4 22 12.6 21.9L19.8 17.7C20.3 17.4 20.6 16.8 20.6 16.2V7.9C20.6 7.3 20.3 6.7 19.8 6.4L12.6 2.2C12.4 2.1 12.1 2 11.8 2Z" fill="currentColor" />
-                              <path d="M11.8 8.69995L8.90001 10.3V13.7L11.8 15.3L14.7 13.7V10.3L11.8 8.69995Z" fill="currentColor" />
-                          </svg>
+                        <i class="fa-solid fa-database"></i>
                       </span>
                       <!--end::Svg Icon-->
                   </span>
-                  <span class="menu-title">Animes</span>
+                  <span class="menu-title">List Animes</span>
               </router-link>
               <!--end:Menu link-->
 
@@ -84,30 +81,26 @@ import { ref, watch } from 'vue';
                   <span class="menu-icon">
                       <!--begin::Svg Icon | path: icons/duotune/abstract/abs014.svg-->
                       <span class="svg-icon svg-icon-2">
-                          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path opacity="0.3" d="M11.8 5.2L17.7 8.6V15.4L11.8 18.8L5.90001 15.4V8.6L11.8 5.2ZM11.8 2C11.5 2 11.2 2.1 11 2.2L3.8 6.4C3.3 6.7 3 7.3 3 7.9V16.2C3 16.8 3.3 17.4 3.8 17.7L11 21.9C11.3 22 11.5 22.1 11.8 22.1C12.1 22.1 12.4 22 12.6 21.9L19.8 17.7C20.3 17.4 20.6 16.8 20.6 16.2V7.9C20.6 7.3 20.3 6.7 19.8 6.4L12.6 2.2C12.4 2.1 12.1 2 11.8 2Z" fill="currentColor" />
-                              <path d="M11.8 8.69995L8.90001 10.3V13.7L11.8 15.3L14.7 13.7V10.3L11.8 8.69995Z" fill="currentColor" />
-                          </svg>
+                        <i class="fa-solid fa-database"></i>
                       </span>
                       <!--end::Svg Icon-->
                   </span>
-                  <span class="menu-title">Episodes</span>
+                  <span class="menu-title">List Episodes</span>
               </router-link>
               <!--end:Menu link-->
 
-             <!--begin:Menu link-->
-             <a class="menu-link" href="">
-                <span class="menu-icon">
-                  <span class="svg-icon svg-icon-2">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path opacity="0.3" d="M11.8 5.2L17.7 8.6V15.4L11.8 18.8L5.90001 15.4V8.6L11.8 5.2ZM11.8 2C11.5 2 11.2 2.1 11 2.2L3.8 6.4C3.3 6.7 3 7.3 3 7.9V16.2C3 16.8 3.3 17.4 3.8 17.7L11 21.9C11.3 22 11.5 22.1 11.8 22.1C12.1 22.1 12.4 22 12.6 21.9L19.8 17.7C20.3 17.4 20.6 16.8 20.6 16.2V7.9C20.6 7.3 20.3 6.7 19.8 6.4L12.6 2.2C12.4 2.1 12.1 2 11.8 2Z" fill="currentColor" />
-                        <path d="M11.8 8.69995L8.90001 10.3V13.7L11.8 15.3L14.7 13.7V10.3L11.8 8.69995Z" fill="currentColor" />
-                    </svg>
-                  </span>
+            <!--begin:Menu link-->
+            <router-link to="/users" class="menu-link" :class="{ 'active': active?.name === 'users-page' }">
+              <span class="menu-icon">
+                <!--begin::Svg Icon | path: icons/duotune/abstract/abs014.svg-->
+                <span class="svg-icon svg-icon-2">
+                  <i class="fa-solid fa-gear"></i>
                 </span>
-                <span class="menu-title">Users</span>
-              </a>
-              <!--end:Menu link-->
+                <!--end::Svg Icon-->
+              </span>
+              <span class="menu-title">Manage User</span>
+            </router-link>
+            <!--end:Menu link-->
 
           </div>
           <!--end:Menu item-->																																										
