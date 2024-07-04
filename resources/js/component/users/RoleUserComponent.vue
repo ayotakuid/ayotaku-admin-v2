abc47a9ac9eda2fefe66cd1aab112d5c44eb5b7d
Rezu0 <yunianrezky031@gmail.com>
2024-07-05 01:16:23 +0700

    component untuk role user

---
+++ resources/js/component/users/RoleUserComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref, watchEffect } from 'vue';
+
+  const props = defineProps(['propsData']);
+  const dataRole = ref();
+
+  watchEffect(() => {
+    dataRole.value = props.propsData?.role;
+  })
+</script>
+
+<template>
+  <span
+    class="badge mx-1 text-center fs-6"
+    :class="{
+      'badge-success': dataRole === 'admin',
+      'badge-danger': dataRole === 'user'
+    }"
+  >
+    {{ dataRole }}
+  </span>
+</template>
