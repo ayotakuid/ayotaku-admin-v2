98c8a94a04e2b165c9248ff88df57fa896040e32
Rezu0 <yunianrezky031@gmail.com>
2024-10-08 13:07:22 +0700

    add: component page untuk recommend anime recommend

---
+++ resources/js/component/animes/RecommendAnimeCompoent.vue
@@ -0,0 +1,33 @@
+<script setup>
+  const data = [
+    {
+      name: 'recommend-page',
+      text: 'Recommend Anime Page',
+    },
+  ];
+
+  const emit = defineEmits(['parents'])
+  emit('parents', data);
+</script>
+
+<template>
+  <div class="row g-5 g-xl-10 mb-5 mb-xl-10">
+    <div class="card shadow-sm">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 my-2 d-flex justify-content-end">
+              <router-link
+                to="/recommend/add"
+                class="btn btn-success btn-sm btn-icon"
+                v-tooltip.bottom-end="'Edit Recommend Anime'"
+              >
+                <i class="fa-solid fa-plus"></i>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
